1e2f22f62af6d3afe95aceaeb15f1092d4aad58c
EGTECHNOLOGY\aishwaryapattar <aishwaryapattar@egtechnology.co.uk>
2023-12-08 12:45:27 +0000

    implementation for day 8 part 2 but not working yet due to starting point iterators

--- Day8/Day8.cpp
+++ Day8/Day8.cpp
@@ -3,16 +3,21 @@
 #include <string>
 #include <fstream>
 #include <tuple>
+#include <vector>
 
 using namespace std;
 
 // function prototype
+void Part1(string filename, string instructions);
+void Part2(string filename, string instructions);
 map<string, tuple<string,string>> GetMap(string filename);
-int FindPath(string instructions, map<string, tuple<string,string>> map);
+int FindPath1(string instructions, map<string, tuple<string,string>> map);
+int FindPath2(string instructions, map<string, tuple<string,string>> map, vector<std::map<string, tuple<string, string>>::iterator> positions);
+vector<std::map<string, tuple<string, string>>::iterator> GetStartingPoints(map<string, tuple<string,string>> map);
+bool IsEndReached(vector<std::map<string, tuple<string, string>>::iterator> positions);
 
-int main() {
-    // notes: have a map with the key: string value:Tuple<string,string>
-
+int main() 
+{
     // get file
     string filename = "PuzzleFile.txt";
     std::ifstream file(filename);
@@ -21,15 +26,28 @@ int main() {
     // get instructions from the file - they exist in the first line
     string instructions;
     getline(file, instructions);
-    
-    map<string, tuple<string,string>> map = GetMap(filename);
-    int numSteps = FindPath(instructions, map);
 
-    cout << "Number of steps from AAA to ZZZ: " << numSteps <<endl;
+    Part2(filename, instructions);
     return 0;
 }
 
 // define functions
+void Part1(string filename, string instructions)
+{
+    map<string, tuple<string,string>> map = GetMap(filename);
+    int numSteps = FindPath1(instructions, map);
+    cout << "Number of steps from AAA to ZZZ: " << numSteps <<endl;
+}
+
+void Part2(string filename, string instructions)
+{
+    map<string, tuple<string,string>> map = GetMap(filename);
+    auto startingPoints = GetStartingPoints(map);
+    int numSteps = FindPath2(instructions, map, startingPoints);
+
+    cout << "Number of steps from AAA to ZZZ: " << numSteps <<endl;
+}
+
 map<string, tuple<string,string>> GetMap(string filename)
 {
     std::ifstream file(filename);
@@ -53,7 +71,7 @@ map<string, tuple<string,string>> GetMap(string filename)
     return map;
 }
 
-int FindPath(string instructions, map<string, tuple<string,string>> map)
+int FindPath1(string instructions, map<string, tuple<string,string>> map)
 {
     // find start position - AAA
     auto search = map.find("AAA");
@@ -78,6 +96,65 @@ int FindPath(string instructions, map<string, tuple<string,string>> map)
             iter = 0;
         }
     }
-
     return numSteps;
+}
+
+int FindPath2(string instructions, map<string, tuple<string,string>> map, vector<std::map<string, tuple<string, string>>::iterator> positions)
+{
+    int numSteps = 0;
+    int iter = 0;
+
+    while (!IsEndReached(positions))
+    {
+        if (iter < instructions.size())
+        {
+            // iterator is smaller than the size of the instructions string so we can use it
+            for (int i = 0; i < positions.size(); i++)
+            {
+                string nextKey = instructions[iter] == 'L'
+                ? get<0>(positions[i]->second)
+                : get<1>(positions[i]->second);
+
+                // replace the position
+                positions[i] = map.find(nextKey);
+            }
+            
+            numSteps++;
+            iter ++;
+        }
+        else
+        {
+            iter = 0;
+        }
+    }
+    return numSteps;
+}
+
+vector<std::map<string, tuple<string, string>>::iterator> GetStartingPoints(map<string, tuple<string,string>> map)
+{
+    vector<std::map<string, tuple<string, string>>::iterator> startingPoints;
+    auto it = map.begin();
+
+    while (it != map.end()) 
+    { 
+        if ((it->first)[2] == 'A') // last of the 3 letters in the key should end in 'A' to be a starting point
+        {
+            startingPoints.push_back(it);
+        } 
+        it++;
+    } 
+    return startingPoints;
+}
+
+bool IsEndReached(vector<std::map<string, tuple<string, string>>::iterator> positions)
+{
+    bool endReached = true;
+    for (auto position : positions)
+    {
+        if ((position->first)[2] != 'Z')
+        {
+            return false;
+        }
+    }
+    return true;
 }
