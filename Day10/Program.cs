44e26b6ef1f47d7102fabfbee33a7a3c8e0bf59d
EGTECHNOLOGY\aishwaryapattar <aishwaryapattar@egtechnology.co.uk>
2023-12-11 00:55:05 +0000

    Test 4 works but not input file

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Numerics;
 
 namespace Day9
@@ -17,19 +18,28 @@ namespace Day9
         {
             public char Char;
             public Tuple<int, int> Position;
+            public Direction Direction;
         }
+
+        private static char[,] inputFile;
 
         static void Main(string[] args)
         {
-            var filename = "Test2.txt";
+            var filename = "PuzzleInput.txt";
             var lines = File.ReadAllLines(filename);
 
             // Part 1
             var validRoute = GetValidLoop(lines);
 
             // Part2
-            var numInside = GetTilesInsideRoute(validRoute, lines);
+            ConvertLinesToMatrix(lines);
+            ReplaceNonRouteCells(validRoute);
+            PrintMatrix();
+
+            var numInside = GetTilesInsideRoute(validRoute);
             Console.WriteLine($"Tiles inside route = {numInside}");
+
+            //PrintMatrix();
         }
         static List<CellInfo> GetValidLoop(string[] lines)
         {
@@ -147,10 +157,10 @@ namespace Day9
                 // check if the character in the next position is valid
                 if (validChars.Contains(nextChar))
                 {
-                    path.Add(new CellInfo {Char = nextChar, Position = nextPos});
                     direction = GetNextDirection(nextChar, direction);
                     currentRow = nextPos.Item1;
                     currentCol = nextPos.Item2;
+                    path.Add(new CellInfo { Char = nextChar, Position = nextPos, Direction = direction });
                 }
                 else
                 {
@@ -198,78 +208,159 @@ namespace Day9
             throw new ArgumentOutOfRangeException(nameof(currentDirection), currentDirection, null);
         }
 
-        static int GetTilesInsideRoute(List<CellInfo> route, string[] lines)
+        static void ConvertLinesToMatrix(string[] lines)
         {
-            var numRows = lines.Count();
-            var numCols = lines[0].Length;
-            var numInside = 0;
+            var rows = lines.Length;
+            var cols = lines[0].Length;
 
-            // iterate through each cell in the file and identify whether it is inside the loop or not
-            for (var row = 0; row < numRows; row++)
+            char[,] matrix = new char[rows, cols];
+
+            for (var row = 0; row < rows; row++)
             {
-                for (var col = 0; col < numCols; col++)
+                for (var col = 0; col < cols; col++)
                 {
-                    // first check that it isn't a cell on the route
-                    if (!IsCellOnRoute(route, row, col))
+                    matrix[row,col] = (lines[row])[col];
+                }
+            }
+            inputFile = matrix;
+        }
+
+        static int GetTilesInsideRoute(List<CellInfo> route)
+        {
+            var numI = 0;
+            var numO = 0;
+
+            // using the right hand rule. Assuming the path is going clockwise, label everything on the left as O and on the right as I
+            // handle the S last as we don't know what the symbol is and it is likely that at the end, all the O's and I's are populated
+            for (var i = 1; i < route.Count; i++) // omit the first S
+            {
+                if (route[i].Char == '|')
+                {
+                    if (route[i].Direction == Direction.Up)
                     {
-                        // if cell isn't on route, identify whether it is inside or not
-                        // can determine this by how many times it intersects the route until it gets to the edge
-                        // odd number = inside. 0 or even number = outside
-                        // intersections need to be perpendicular - do not follow the length of a route
-                        //      therefor exclude '|' if we are following in vertical direction  
-                        //      exclude '-' if we are following in horizontal direction
-                        // for ease, we are going to follow all cells vertically upwards to see how many times they intersect
-                        
-                        if (row != 0) // if row is 0, it will be on the outside
-                        {
-                            var numIntersections = 0;
-                            //var numJunctions = 0;
-
-                            // check whether rows above the cell intersect the route
-                            for (int i = row - 1; i >= 0; i--)
-                            {
-                                if (IsCellOnRoute(route, i, col))
-                                {
-                                    char c = (lines[i])[col];
-                                    if (c == '-')
-                                    {
-                                        numIntersections++;
-                                    }
-
-
-                                    //// confirm it is not following the path
-                                    //if (c != '|')
-                                    //{
-                                    //    if (c == '-')
-                                    //    {
-                                    //        // perpendicular intersection
-                                    //        numIntersections++;
-                                    //    }
-                                    //    else
-                                    //    {
-                                    //        // it has crossed a junction - need to find the pair to complete a route intersection
-                                    //        numJunctions++;
-
-                                    //        if (numJunctions == 2)
-                                    //        {
-                                    //            numIntersections++;
-                                    //            numJunctions = 0;
-                                    //        }
-                                    //    }
-                                    //}
-                                }
-                            }
-
-                            if (numIntersections % 2 == 1)
-                            {
-                                numInside++;
-                            }
-                        }
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Right, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Left, route);
                     }
+                    else if (route[i].Direction == Direction.Down)
+                    {
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Left, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Right, route);
+                    }
+                    else { throw new Exception("Invalid direction for |"); }
+                }
+                else if (route[i].Char == '-') 
+                {
+                    if (route[i].Direction == Direction.Right)
+                    {
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Down, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Up, route);
+                    }
+                    else if (route[i].Direction == Direction.Left)
+                    {
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Up, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Down, route);
+                    }
+                    else { throw new Exception("Invalid direction for -"); }
+                }
+                else if (route[i].Char == 'J')
+                {
+                    if (route[i].Direction == Direction.Up)
+                    {
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Right, route);
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Down, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Left, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Up, route);
+                    }
+                    else if (route[i].Direction == Direction.Left)
+                    {
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Up, route);
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Left, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Down, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Right, route);
+                    }
+                    else { throw new Exception("Invalid direction for J"); }
+                }
+                else if (route[i].Char == 'F')
+                {
+                    if (route[i].Direction == Direction.Right)
+                    {
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Down, route);
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Right, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Up, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Left, route);
+                    }
+                    else if (route[i].Direction == Direction.Down)
+                    {
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Left, route);
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Up, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Right, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Down, route);
+                    }
+                    else { throw new Exception("Invalid direction for F"); }
+                }
+                else if (route[i].Char == 'L')
+                {
+                    if (route[i].Direction == Direction.Up)
+                    {
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Right, route);
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Up, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Left, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Down, route);
+                    }
+                    else if (route[i].Direction == Direction.Right)
+                    {
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Down, route);
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Left, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Up, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Right, route);
+                    }
+                    else { throw new Exception("Invalid direction for L"); }
+                }
+                else if (route[i].Char == '7')
+                {
+                    if (route[i].Direction == Direction.Down)
+                    {
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Left, route);
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Down, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Right, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Up, route);
+                    }
+                    else if (route[i].Direction == Direction.Left)
+                    {
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Up, route);
+                        numI += PopulateCellsWithChar('I', route[i].Position, Direction.Right, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Down, route);
+                        numO += PopulateCellsWithChar('O', route[i].Position, Direction.Left, route);
+                    }
+                    else { throw new Exception("Invalid direction for 7"); }
+                }
+                else if (route[i].Char == 'S') {/*ignore*/}
+                else {throw new Exception("Invalid character"); }
+            }
+
+            // figure out whether I or O corresponds to the outside - look along the first row for I or O
+            var outsideChar = ' ';
+            for (var i = 0; i < inputFile.GetLength(1); i++)
+            {
+                if (inputFile[0,i] == 'I')
+                {
+                    outsideChar = 'I';
+                    Console.WriteLine("Outside char is I");
+                    break;
+                }
+
+                if (inputFile[0, i] == 'O')
+                {
+                    outsideChar = 'O';
+                    Console.WriteLine("Outside char is O");
+                    break;
                 }
             }
 
-            return numInside;
+            // return the INSIDE
+            if (outsideChar == 'O') return numI;
+            if (outsideChar == 'I') return numO;
+            throw new Exception("Didn't find an O or I on the outside");
         }
 
         // row and col corresponds to the cell we want to compare and check if it exists on the route
@@ -284,6 +375,103 @@ namespace Day9
                 }
             }
             return false;
+        }
+
+        static int PopulateCellsWithChar(char populateChar, Tuple<int, int> currentPos, Direction direction, 
+            List<CellInfo> route)
+        {
+            var numRows = inputFile.GetLength(0);
+            var numCols = inputFile.GetLength(1);
+            var numCharsWritten = 0;
+
+            // populate cells until we hit the route, another element of the same character, or the edge of the board
+            switch (direction)
+            {
+                case Direction.Up:
+                    for (var row = currentPos.Item1 - 1; row >= 0; row--)
+                    {
+                        if (IsCellOnRoute(route, row, currentPos.Item2)
+                            || inputFile[row, currentPos.Item2] == populateChar)
+                        {
+                            break; // found end point of populating
+                        }
+                        inputFile[row, currentPos.Item2] = populateChar;
+                        numCharsWritten++;
+                    }
+                    break;
+                case Direction.Right:
+                    for (var col = currentPos.Item2 + 1; col < numCols; col++)
+                    {
+                        if (IsCellOnRoute(route, currentPos.Item1, col)
+                            || inputFile[currentPos.Item1, col] == populateChar)
+                        {
+                            break; // found end point of populating
+                        }
+                        inputFile[currentPos.Item1, col] = populateChar;
+                        numCharsWritten++;
+                    }
+                    break;
+                case Direction.Down:
+                    for (var row = currentPos.Item1 + 1; row < numRows; row++)
+                    {
+                        if (IsCellOnRoute(route, row, currentPos.Item2)
+                            || inputFile[row, currentPos.Item2] == populateChar)
+                        {
+                            break; // found end point of populating
+                        }
+                        inputFile[row, currentPos.Item2] = populateChar;
+                        numCharsWritten++;
+                    }
+                    break;
+                case Direction.Left:
+                    for (var col = currentPos.Item2 - 1; col >= 0; col--)
+                    {
+                        if (IsCellOnRoute(route, currentPos.Item1, col)
+                            || inputFile[currentPos.Item1, col] == populateChar)
+                        {
+                            break; // found end point of populating
+                        }
+                        inputFile[currentPos.Item1, col] = populateChar;
+                        numCharsWritten++;
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+            }
+
+            return numCharsWritten;
+        }
+
+        static void ReplaceNonRouteCells(List<CellInfo> route)
+        {
+            var numRows = inputFile.GetLength(0);
+            var numCols = inputFile.GetLength(1);
+
+            for (int i = 0; i < numRows; i++)
+            {
+                for (int j = 0; j < numCols; j++)
+                {
+                    if (!IsCellOnRoute(route, i, j))
+                    {
+                        inputFile[i, j] = '.';
+                    }
+                }
+            }
+        }
+
+        static void PrintMatrix()
+        {
+            var numRows = inputFile.GetLength(0);
+            var numCols = inputFile.GetLength(1);
+
+            for (int i = 0; i < numRows; i++)
+            {
+                for (int j = 0; j < numCols; j++)
+                {
+                    Console.Write($"{inputFile[i,j]}" );
+                }
+                Console.Write("\n");
+            }
         }
     }
 }
