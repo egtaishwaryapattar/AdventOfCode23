bba57765c8d3af198f5f4dc0e54b62b177614487
EGTECHNOLOGY\aishwaryapattar <aishwaryapattar@egtechnology.co.uk>
2023-12-16 10:15:55 +0000

    a start on day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Numerics;
+
+namespace Day3
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "Test1.txt";
+            
+        }
+    }
+}
